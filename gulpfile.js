55c24623ecad89813fa516ba7dbe2e6044ba3257
Kurt Jonach <kurtjonach@Wotan.local>
2017-07-12 21:21:16 -0700

    Grid layout

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,18 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+var cssnano = require('gulp-cssnano');
+var del = require('del');
+var runSequence = require('run-sequence');
 
 gulp.task('default', ['watch']);
 
 gulp.task('watch', ['browserSync', 'sass'], function() {
     gulp.watch('src/scss/**/*.scss', ['sass']);
     gulp.watch('src/*.html', browserSync.reload);
-    gulp.watch('src/js/**/*.js', browserSync.reload);
 });
 
 gulp.task('browserSync', function() {
@@ -26,3 +31,32 @@ gulp.task('sass', function() {
             stream: true
         }));
 });
+
+gulp.task('grid', function() {
+    return gulp.src('src/grid/scss/grid.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('src/grid/css'));
+});
+
+////////// COMBINE & MINIFY
+gulp.task('minify', function() {
+    return gulp.src('src/*.html')
+      .pipe(useref())
+      // minify only if JavaScript
+      .pipe(gulpIf('*.js', uglify()))
+      // minify only if CSS
+      .pipe(gulpIf('*.css', cssnano()))
+      .pipe(gulp.dest('dist'))
+});
+
+////////// CLEAN DISTRIBUTION FOLDER
+gulp.task('clean', function() {
+  return del.sync('dist');
+});
+
+////////// BUILD DISTRIBUTION FOLDER
+gulp.task('build', function (callback) {
+  runSequence('clean', ['sass', 'minify'],
+    callback
+  )
+})
